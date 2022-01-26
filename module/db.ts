bdedb1c5ef934609983a70035f91990b061366c1
muhammad al ghifari <muhamadalghifari97@gmail.com>
2022-01-26 15:27:24 +0700

    new

--- module/db.ts
+++ module/db.ts
@@ -1,6 +1,4 @@
 import { MongoClient } from "mongodb";
-
-// const url = "mongodb+srv://ghifari:alghi7198254@democluster.hfncl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const url = process.env.MONGO_URI;
 
 const client = new MongoClient(url)
